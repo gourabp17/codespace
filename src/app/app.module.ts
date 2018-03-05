8802439e7c2510e02341be4e1a0f988f4dac3c36
Gourab Paul <gourab.p17@gmail.com>
2018-03-05 19:05:25 +0530

    added img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule, MatToolbarModule,
+  MatFormFieldModule, MatInputModule, MatSnackBarModule, MatTableModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -14,6 +20,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule,
+    MatToolbarModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatTableModule,
     AppRoutingModule
   ],
   providers: [],
